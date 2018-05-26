5f3b7fb1e91beccaa1e85ff9b7595bd5ee9371a5
mnv <mnvx@yandex.ru>
2018-05-26 16:34:45 +0300

    Gitlab host as env parameter

--- config/gitlab.php
+++ config/gitlab.php
@@ -1,13 +1,15 @@
 <?php
 
+$gitlabHost = env('GITLAB_HOST', 'https://gitlab.com/');
+
 return [
 
     'urls' => [
-        'project-list' => 'https://gitlab.com/api/v3/projects',
-        'project-item' => 'https://gitlab.com/api/v3/projects/:project_id',
-        'project-issue-list' => 'https://gitlab.com/api/v3/projects/:project_id/issues',
-        'project-issue-item' => 'https://gitlab.com/api/v3/projects/:project_id/issues/:issue_id',
-        'project-issue-note-list' => 'https://gitlab.com/api/v3/projects/:project_id/issues/:issue_id/notes',
+        'project-list' => $gitlabHost . 'api/v3/projects',
+        'project-item' => $gitlabHost . 'api/v3/projects/:project_id',
+        'project-issue-list' => $gitlabHost . 'api/v3/projects/:project_id/issues',
+        'project-issue-item' => $gitlabHost . 'api/v3/projects/:project_id/issues/:issue_id',
+        'project-issue-note-list' => $gitlabHost . 'api/v3/projects/:project_id/issues/:issue_id/notes',
     ],
 
     'token' => env('GITLAB_PRIVATE_TOKEN'),
