3e9d8ff6352dd0a3c8dcd98a1fd0392f72c4b2cf
mnv <mnvx@yandex.ru>
2019-06-09 01:13:55 +0300

    Updated js dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,6 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .copy('node_modules/moment/min/moment-with-locales.js', 'public/js/moment-with-locales.js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css', {
+       implementation: require('node-sass')
+   });
